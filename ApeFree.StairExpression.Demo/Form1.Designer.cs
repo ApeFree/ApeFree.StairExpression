74d2b625f872b6503dee42619cafe22d1ed84600
Guijie Lee <landriesnidis@yeah.net>
2022-10-21 01:07:37 +0800

    Html查询Demo

---
+++ ApeFree.StairExpression.Demo/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ApeFree.StairExpression.Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbExpression = new System.Windows.Forms.TextBox();
+            this.tbHtml = new System.Windows.Forms.RichTextBox();
+            this.tbResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tbExpression
+            // 
+            this.tbExpression.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tbExpression.Location = new System.Drawing.Point(0, 0);
+            this.tbExpression.Name = "tbExpression";
+            this.tbExpression.Size = new System.Drawing.Size(1113, 21);
+            this.tbExpression.TabIndex = 0;
+            this.tbExpression.Text = "html.head.m*[name=*]";
+            this.tbExpression.TextChanged += new System.EventHandler(this.tbExpression_TextChanged);
+            // 
+            // tbHtml
+            // 
+            this.tbHtml.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tbHtml.Location = new System.Drawing.Point(0, 21);
+            this.tbHtml.Name = "tbHtml";
+            this.tbHtml.Size = new System.Drawing.Size(636, 564);
+            this.tbHtml.TabIndex = 1;
+            this.tbHtml.Text = resources.GetString("tbHtml.Text");
+            this.tbHtml.WordWrap = false;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbResult.Location = new System.Drawing.Point(636, 21);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(477, 564);
+            this.tbResult.TabIndex = 2;
+            this.tbResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 585);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbHtml);
+            this.Controls.Add(this.tbExpression);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbExpression;
+        private System.Windows.Forms.RichTextBox tbHtml;
+        private System.Windows.Forms.RichTextBox tbResult;
+    }
+}
+
---
+++ ApeFree.StairExpression.Demo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using ApeFree.StairExpression.Html;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApeFree.StairExpression.Demo
+{
+    public partial class Form1 : Form
+    {
+        HtmlAgilityPack.HtmlDocument doc;
+        public Form1()
+        {
+            InitializeComponent();
+
+            doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(tbHtml.Text);
+        }
+
+        private void tbExpression_TextChanged(object sender, EventArgs e)
+        {
+            tbResult.Clear();
+
+            try
+            {
+
+
+                var result = HtmlStairActuator.Query(doc, tbExpression.Text);
+
+                StringBuilder sb = new StringBuilder($"共查询到[{result.Nodes.Count}]条结果：\r\n\r\n");
+                int index = 1;
+                foreach (var node in result.Nodes)
+                {
+                    sb.AppendLine($"---------------- 结果{index++} ----------------");
+                    sb.AppendLine(node.Node.OuterHtml.Trim());
+                    //sb.AppendLine(node.Node.GetPath());
+                    //sb.AppendLine(node.Node.XPath);
+                    sb.AppendLine();
+                }
+
+                tbResult.Text = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                tbResult.Text = ex.Message;
+            }
+        }
+    }
+}
