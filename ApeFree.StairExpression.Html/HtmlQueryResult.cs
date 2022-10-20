31ea9e72d43a44e2b50387179830569ea00ae754
Guijie Lee <landriesnidis@yeah.net>
2022-10-21 01:03:56 +0800

    Html节点查询结果类型

---
+++ ApeFree.StairExpression.Html/HtmlQueryResult.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using System.Xml.Linq;
+
+namespace ApeFree.StairExpression.Html
+{
+    public class HtmlQueryResult : QueryResult<HtmlNode>
+    {
+        internal HtmlQueryResult(Statement statement) : base(statement)
+        {
+        }
+
+        /// <summary>
+        /// 添加结果节点
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="statementNode"></param>
+        internal void AddNode(HtmlNode node, StatementNode statementNode)
+        {
+            nodes.Add(new QueryResultNode<HtmlNode>(node, statementNode));
+        }
+    }
+}
